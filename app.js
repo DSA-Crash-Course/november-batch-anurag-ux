19f0958e10867a6f2610d2779b3d4a1aa115453b
Anurag <anurag.s@refactor.academy>
2022-11-16 10:21:06 +0530

    submitting assignment 1. completed 2  tasks out of 5

---
+++ app.js
@@ -0,0 +1,11 @@
+var a = 10;
+var str = "hello";
+
+var flags = true;
+
+flag = !flag;
+
+//      push
+//add -> adds my files to the staging area
+//commit -> bind all my files together into a single commit
+//push -> add all our commits to the online repository(github)
